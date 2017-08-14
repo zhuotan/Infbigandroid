ca5b9fcb664df80ec0d3cce34d750c583099ded1
tanzhuo <tanzhuo_git@sina.cn>
2017-08-14 09:28:00 +0800

    first import code

---
+++ app/src/main/java/rednum/com/infbigand/MainActivity.java
@@ -0,0 +1,13 @@
+package rednum.com.infbigand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
