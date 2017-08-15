1cacc350e343bf0877e6b831a88dc5d5d52ef7f4
tanzhuo <tanzhuo_git@sina.cn>
2017-08-15 16:31:57 +0800

    first import code step2

--- app/src/main/java/rednum/com/infbigand/MainActivity.java
+++ app/src/main/java/rednum/com/infbigand/MainActivity.java
@@ -1,13 +1,22 @@
 package rednum.com.infbigand;
 
+import android.app.Activity;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends  Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         setContentView(R.layout.activity_main);
     }
 }
